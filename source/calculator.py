021ce03eae7a124eec96f02ef9b94371fec855d5
tok33 <99845530+tok33@users.noreply.github.com>
2023-02-16 16:58:11 -0500

    Add files via upload

---
+++ source/calculator.py
@@ -0,0 +1,7 @@
+try:
+  num1 = int(input("Enter your first number: "))
+  num2 = int(input("Enter your second number: "))
+
+  print("The sum of these numbers is:", num1 + num2)
+except:
+  print("You didn't enter a valid number :(")
